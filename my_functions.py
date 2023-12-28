12320eadf1c4a2e2951bd72db1dfc7eb0d78e8ec
heyscottn <heyscottn@gmail.com>
2023-12-28 11:37:44 -0600

    edits

--- my_functions.py
+++ my_functions.py
@@ -1,20 +1,20 @@
 # if the input is a vertical list of items, one per line
 # this will return a list of integers
 def getintlist(filename):
-  my_list = []
-  f = open(filename)
+    my_list = []
+    f = open(filename)
 
-  for line in f:
-    my_list.append(int(line.strip()))
-  f.close()
+    for line in f:
+        my_list.append(int(line.strip()))
+    f.close()
 
-  return my_list
+    return my_list
 
 
 def getstring(filename):
-  f = open(filename, "r")
-  ans = f.read()
-  f.close()
-  return ans
+    f = open(filename, "r")
+    ans = f.read()
+    f.close()
+    return ans
 
 
