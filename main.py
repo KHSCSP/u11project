77966de1aac0b58d7265ed83ea61551fe53ddb8b
me_chromebook <heyscottn@gmail.com>
2024-01-06 08:40:52 -0600

    edits

--- main.py
+++ main.py
@@ -1,66 +1,52 @@
-#!bin/python3
 # ---- do not edit this section ----
 print("preparing your variables...")
 import my_functions as myf
-nums = myf.getintlist("u11project/my_nums.txt")
+scores = myf.getintlist("u11project/act_data.txt")
 words = myf.getstring("u11project/my_story.txt")
 # --- do not edit above this line ---
 
 print("\n--- part 1, becoming familiar with the data ---")
-# TODO print 'nums' to see what you are dealing with, then comment out this line
+# TODO print 'scores' to see what you are dealing with, then comment out this line
 # TODO print 'words' to see what you are dealing with, then comment out this line
 
 
-# TODO what type of data is 'nums'? (print its type)
+# TODO what type of data is 'scores'? (print its type)
 # TODO what type of data is 'words'? (print its type)
 
 
 
-# TODO how many items are in 'nums'?
+# TODO how many items are in 'scores'?
 # TODO how many items are in 'words'?
 
 
 
-# TODO print the first and last item in 'nums'
+# TODO print the first and last item in 'scores'
 # TODO print the first and last item in 'words'
 
 
 
-# TODO print an item near the middle of 'nums'
+# TODO print an item near the middle of 'scores'
 # TODO print an item near the middle of 'words'
 
 
 
-# TODO use slicing to print the first 10 items in 'nums'
+# TODO use slicing to print the first 10 items in 'scores'
 # TODO use slicing to print the last 10 items in 'words'
 
 
 
-# TODO if 13 is in nums, print its index
+# TODO if 36 is in scores, print its index
 # TODO if "computer" is in words, print its index
 
 
 
 
 
-#---------- part 2, focus on 'nums' -----------------------
-print("\n\n--- part 2, focus on 'nums' ---")
-# The data for 'nums' was corrupted!
-# the data should not have any numbers less than 10
-# TODO count how many numbers are less than 10
-
-
-# TODO create a new list with only items greater than or equal to 10
-
-
-
-# TODO display how many items are in this new list
-
-
-
-# TODO print the sum of the items in 'nums'
-# TODO print the largest and smallest item in 'nums'
-# TODO sort 'nums', display the first and last item to check your work from above
+#---------- part 2, focus on 'scores' -----------------------
+print("\n\n--- part 2, focus on 'scores' ---")
+# TODO determine the average of 'scores' (remember: avg = sum/number_of_items)
+# TODO print the largest and smallest item in 'scores'
+# TODO sort 'scores', display the first and last item to check your work from above
 
 
 
@@ -69,6 +55,8 @@ print("\n\n--- part 2, focus on 'nums' ---")
 # TODO make a list of tuples where each tuple is (i, count(i))
 # hint: what are the min & max values? this is the range for 'i'
 
+
+# perform one more data analysis of your choice on 'scores'
 
 
 
